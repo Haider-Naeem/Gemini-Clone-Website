70a6b0c20a74e12a9248c288b3e19dfc136d499b
Haider-Naeem <haidern0203@gmail.com>
2025-06-26 08:02:08 +0500

    Final UI Complete

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import "./Main.css"
+import "./Main.css";
+import { assets } from "../../assets/assets";
 
-const Main = () => {
+function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello Haider</span>
+          </p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a prompt here ...." />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info,so double check its response.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
